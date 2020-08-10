f0895d0628c835166abad5111973ceeaead4d73c
HerendraTJ <herendra.kurniawan@gmail.com>
2020-08-10 23:22:52 +0700

    Finishing Library ball.h
    Finishing library for ball.h

--- ball.h
+++ ball.h
@@ -2,13 +2,29 @@
 #define PONG_BALL_H
 #include <ncurses.h>
 
-struct ball{
+class ball{
 public:
     ball(double x,double y, int speed);
     double getX(){return x;}
     double getY() {return y;}
+    int getSpeed() {return speed;}
 
 
+    void setX (double a){
+        x=a;
+}
 
 
+void setY(double a){
+y=a;
+}
+
+void setSpeed (int a){
+mvaddch(x,y,'o');
+}
+private:
+    double x,y;
+    int speed;
 };
+
+#endif
