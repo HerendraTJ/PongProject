c5d1b8df06d27ce559f37f83fdfc872c40b28e16
HerendraTJ <herendra.kurniawan@gmail.com>
2020-08-10 23:36:15 +0700

    Change to main game
    Working on the game , and also creating cpp for the library

---
+++ ball.cpp
@@ -0,0 +1,7 @@
+#include"ball.h"
+
+ball::ball(double x,double y,int speed){
+setY(y);
+setX(x);
+setSpeed(speed);
+}
--- pongame.cpp
+++ pongame.cpp
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <ncurses.h>
-#include //Library player disini custom made
-#include //Library ball disini Custom made
+#include <player.h>
+#include <ball.h>
+#include <conio.h>
+
 
 int ch;
 int panjang=80;
@@ -15,8 +17,47 @@ bool player1Server,player2Serve=false;
 Player player1
 
 int main(){
+setup();
+while(!quit){
+    input();
+    logic();
+    draw();
+}
+endwin();
+return 0;
+}
+
+void setup(){
+wallTexture = '#';
+initscr();
+cbreak();
+noecho();
+curs_set(0);
+keypad(stdscr,true);
+timeout(50);
 
 
+quit = false;
+player1Points=0;
+player2Points=0;
+}
+
+void input(){
+ch=getch();
+switch(ch){
+
+
+
+
+
+}
+
+
+
+
+
+
+}
 
 return 0;
 }
