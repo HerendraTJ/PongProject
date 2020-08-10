90a739ae217f3490248d7d6752f1f555abbc9467
HerendraTJ <herendra.kurniawan@gmail.com>
2020-08-10 12:02:49 +0700

    Early Development
    Baru cuman sekedar bikin variable, masih early development belum jadi

---
+++ pongame.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <ncurses.h>
+#include //Library player disini custom made
+#include //Library ball disini Custom made
+
+int ch;
+int panjang=80;
+int lebar=24;
+int dir=1;
+int player1Points,player2Points=0;
+bool quit;
+char wallTexture,playerTexture;
+
+
+
+int main(){
+
+
+
+return 0;
+}
+
